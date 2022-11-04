e8cab47f2ed78951736dac39f6c75c6ae09d2e49
Ben-Hur <benhurcarvalho2@gmail.com>
2022-11-04 20:03:03 -0300

    requisito 2, renderiza login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends Component {
+  state = {
+    name: '',
+    isBtnDisabled: false,
+    carregando: false,
+    login: false,
+  };
+
+  ValidarBtn = ({ target }) => {
+    // console.log(target);
+    const { value } = target;
+    // console.log(value);
+    const carcterMin = 3;
+    this.setState({
+      name: value,
+      isBtnDisabled: value.length >= carcterMin,
+    });
+  };
+
+  SalveUser = async (ev) => {
+    ev.preventDefault();
+    const { name } = this.state;
+    console.log(name.toLocaleUpperCase());
+    this.setState({
+      carregando: true,
+    });
+    await createUser({ name });
+    this.setState({
+      carregando: false,
+      login: true,
+    });
+  };
+
   render() {
+    const { isBtnDisabled, carregando, login } = this.state;
+    // console.log(carregando);
+    // console.log(login);
+    // console.log(createUser);
+    // console.log(Redirect);
+    // console.log(Carregando);
     return (
       <div data-testid="page-login">
-        Login
+        { carregando ? <Carregando />
+          : (
+            <form className="Login">
+              <input
+                className="nome"
+                type="text"
+                name="name"
+                data-testid="login-name-input"
+                placeholder="Digite seu nome"
+                onChange={ this.ValidarBtn }
+              />
+              <button
+                type="submit"
+                className="btnEntrar"
+                data-testid="login-submit-button"
+                onClick={ this.SalveUser }
+                disabled={ !isBtnDisabled }
+              >
+                Entrar
+              </button>
+            </form>
+          ) }
+        {login && <Redirect to="/search" /> }
       </div>
     );
   }
