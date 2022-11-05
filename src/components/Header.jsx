45080d56eb362f5167350b893b327420fb3ca98a
Ben-Hur <benhurcarvalho2@gmail.com>
2022-11-04 21:58:33 -0300

    completa o requisito 3 e renderizo usando as rotas com o Switch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Carregando from '../pages/Carregando';
 
 class Header extends Component {
+  state = {
+    name: '',
+    carregando: false,
+  };
+
+  componentDidMount() {
+    this.getName();
+  }
+
+  getName = async () => {
+    this.setState({ carregando: true });
+    const use = await getUser();
+    this.setState({
+      name: use.name,
+      carregando: false,
+    });
+  };
+
   render() {
+    const { name, carregando } = this.state;
     return (
       <header data-testid="header-component">
-        Header
+        { carregando ? <Carregando />
+          : (
+            <h3
+              data-testid="header-user-name"
+            >
+              { name }
+            </h3>
+          )}
       </header>
     );
   }
